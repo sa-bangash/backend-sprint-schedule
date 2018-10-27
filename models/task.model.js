0da2435ea0d8bf6009130e4e4f3932cb9cb3a9bf
DESKTOP-UM9UHCK\bangash <shahid@smsami.com>
2018-10-27 23:37:28 +0500

    date validation done

--- models/task.model.js
+++ models/task.model.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var Sprint = require('./sprint.model').SprintModel;
+var moment = require('moment');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -10,9 +12,48 @@ var taskSchema = new Schema({
     storyNumber: { type: String },
     description: { type: String, required: [true, 'Description is required'] },
     estimatedTime: { type: String, required: [true, 'Estimated Time is required'] },
-    date: { type: String, required: [true, 'Date is Required'] },
+    date: {
+        type: String,
+        required: [true, 'Date is Required'],
+        validate: {
+            isAsync: true,
+            validator: function (v, cb) {
+                Sprint.findById(this.sprintId)
+                    .then((value) => {
+                        const start = moment(value.start);
+                        const end = moment(value.end);
+                        const date = moment(v);
+                        if (date.isBetween(start, end,'d','[]')) {
+                            cb(true);
+                        }else{
+                            cb(false);
+                        }
+                    }).catch(function (err) {
+                        cb(false);
+                    })
+
+            },
+            message: 'Date should be exsit within Sprint date',
+        }
+    },
     status: Boolean,
-    sprintId: { type: Schema.Types.ObjectId, ref:'sprint', required: [true, 'Sprint id is requried'] },
+    sprintId: {
+        type: Schema.Types.ObjectId, ref: 'sprint',
+        required: [true, 'Sprint id is required'],
+        validate: {
+            isAsync: true,
+            validator: function (v, cb) {
+                Sprint.findOne({ _id: v }, function (err, doc) {
+                    if (err || !doc) {
+                        cb(false)
+                    } else {
+                        cb(true)
+                    }
+                })
+            },
+            message: 'Sprint id not Exsit'
+        }
+    },
 })
 
 taskSchema.method('toClient', function () {
