ea0a32f5939a76b7fed4ca7dbbd7406c9558301f
DESKTOP-UM9UHCK\bangash <shahid@smsami.com>
2018-11-03 20:06:18 +0500

    creating work space

--- models/user.model.js
+++ models/user.model.js
@@ -3,9 +3,44 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    name: { type: String, required: [true, 'Name is required'] },
+    email: {
+        type: String, required: [true, 'Email is required'],
+        validate: {
+            isAsync: true,
+            validator: function (v, cb) {
+                this.model('User').findOne({ email: v }, function (err, doc) {
+                    console.log(doc);
+                    if (err || doc) {
+                        cb(false)
+                    } else {
+                        cb(true);
+                    }
+
+                })
+            },
+            message: 'Email Already Exsit'
+        }
+    },
+    password: { type: String, required: [true, 'Password is required'] },
+    workSpace: {
+        type: String,
+        validate: {
+            isAsync: true,
+            validator: function (v, cb) {
+                this.model('User').findOne({ workSpace: v }, function (err, doc) {
+                    console.log(doc);
+                    if (err || doc) {
+                        cb(false)
+                    } else {
+                        cb(true);
+                    }
+
+                })
+            },
+            message: 'Work space Already Exsit'
+        }
+    }
 })
 
 UserSchema.method('toClient', function () {
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,7 +2,7 @@ var express = require('express');
 var authRouter = express.Router();
 var jwt = require('jsonwebtoken');
 var UserModel = require('../models/user.model').UserModel;
-
+var validationMessage = require('../gernal/helper').validationMessage;
 var generateToken = require('../config/auth.helper').generateToken;
 
 authRouter.post('/login', function (req, res, next) {
@@ -15,7 +15,9 @@ authRouter.post('/login', function (req, res, next) {
                     email: resp.email,
                 });
             } else {
-                res.status(403).send('You have Invalid email or password');
+                res.status(406).json({
+                    email: 'You have Invalid email or password'
+                });
             }
         })
         .catch((err) => res.status(403).send(err))
@@ -33,10 +35,31 @@ authRouter.post('/signup', function (req, res, next) {
             email: resp.email,
         });
     }).catch(function (err) {
-        res.status(403).send(err);
+        res.status(406).send(err);
     })
 })
 
+authRouter.post('/create-work-space', function (req, res, next) {
+    if (req.body && !req.body.workSpace) {
+        return res.status(406).json({
+            workSpace: 'Work space is Required',
+        })
+    }
+    var newUser = new UserModel({
+        ...req.body,
+    });
+    newUser.save().then(function (resp) {
+        res.json({
+            token: generateToken(resp.toClient()),
+            name: resp.name,
+            email: resp.email,
+            createWorkSpace: res.createWorkSpace,
+        })
+    }).catch(function (err) {
+        console.log(err)
 
+        res.status(406).json(validationMessage(err.errors));
+    })
+})
 
 module.exports = authRouter;
